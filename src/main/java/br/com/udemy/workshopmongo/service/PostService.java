91fd01cad995553cc7dc36eee500c16dfbdf3df3
Marcio Antunes <mantunes001@gmail.com>
2020-01-27 11:31:34 -0300

    Obtendo post por id

---
+++ src/main/java/br/com/udemy/workshopmongo/service/PostService.java
@@ -0,0 +1,23 @@
+package br.com.udemy.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.udemy.workshopmongo.domain.Post;
+import br.com.udemy.workshopmongo.repository.PostRepository;
+import br.com.udemy.workshopmongo.service.exception.ObjectNotfoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotfoundException("Objeto não encontrado")); 
+	}
+}
